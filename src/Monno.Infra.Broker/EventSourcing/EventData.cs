06d88a8583c70a6950b43d85af0519a1c1fb9e33
Hugo Moura <hugo.moura@tecnobank.com.br>
2024-12-11 22:18:09 -0300

    fix: ajustada mensagem de Store Event

--- src/Monno.Infra.Broker/EventSourcing/EventData.cs
+++ src/Monno.Infra.Broker/EventSourcing/EventData.cs
@@ -6,7 +6,7 @@ public class EventData : IIntegrationMessage
 {
     public Guid Id { get; set; }
     public Guid AggregateId { get; set; }
-    public Guid WhoId { get; set; }
+    public Guid ActorId { get; set; }
     public string EventName { get; set; } = string.Empty;
     public string Payload { get; set; } = string.Empty;
     public string AssemblyQualifiedName { get; set; } = string.Empty;
